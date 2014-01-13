32d72ae74ac9a639d3927a8c036236e42f6a5799
Bart <gfoekfi@yahoo.com>
2014-01-13 19:10:47 +0100

    Basic game loop implementation

--- WarZeub2/WarZeub2.cpp
+++ WarZeub2/WarZeub2.cpp
@@ -1,18 +1,63 @@
 #include <SDL.h>
 
+SDL_Surface* screen = 0;
+bool isDone = false;
+
+
+// TODO: Error handling
+bool Init()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_WM_SetCaption("WarZeub 2 !", 0);
+	screen = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE);
+
+	return true;
+}
+
+void Quit()
+{
+	SDL_FreeSurface(screen);
+	SDL_Quit();
+}
+
+void EventHandler(const SDL_Event& parEvent)
+{
+	switch (parEvent.type)
+	{
+	case SDL_KEYDOWN:
+	case SDL_QUIT:
+		isDone = true;
+		break;
+
+	default:
+		break;
+	}
+}
+
+void Render()
+{
+	SDL_Flip(screen);
+}
+
+void Run()
+{
+	SDL_Event event;
+
+	while (!isDone)
+	{
+		SDL_PollEvent(&event);
+		EventHandler(event);
+		Render();		
+	}
+}
 
 int main(int argc, char* argv[])
 {
-	SDL_Init(SDL_INIT_EVERYTHING);
-
-	SDL_Surface* screen = SDL_SetVideoMode(1024, 768, 32, SDL_HWSURFACE);
-
-	SDL_Flip(screen);
-
-	SDL_Delay(2000);
-
-	SDL_FreeSurface(screen);
-	SDL_Quit();
+	if (Init())
+	{
+		Run();
+		Quit();
+	}
 
 	return 0;
 }
