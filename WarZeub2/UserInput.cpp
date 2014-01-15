a14f13b431834a103d4886b746c3704d7c68a6e3
Bart <gfoekfi@yahoo.com>
2014-01-15 12:40:31 +0100

    UserInput: Mouse structure definition + mouse event handler

---
+++ WarZeub2/UserInput.cpp
@@ -0,0 +1,48 @@
+#include "UserInput.h"
+
+
+// ============================================================================
+// ----------------------------------------------------------------------------
+// ============================================================================
+
+Mouse mouse;
+
+// ============================================================================
+// ----------------------------------------------------------------------------
+// ============================================================================
+
+void MouseEventHandler(const SDL_Event& parEvent)
+{
+	switch (parEvent.type)
+	{
+	case SDL_MOUSEBUTTONDOWN:
+		{
+			if (parEvent.button.button == SDL_BUTTON_RIGHT)
+			{
+				mouse.lastRightClickX = parEvent.button.x;
+				mouse.lastRightClickY = parEvent.button.y;
+			}
+			else if (parEvent.button.button == SDL_BUTTON_LEFT)
+			{
+				mouse.leftButtonPressed = true;
+				mouse.lastLeftClickX = parEvent.button.x;
+				mouse.lastLeftClickY = parEvent.button.y;
+			}
+		}
+		break;
+
+	case SDL_MOUSEBUTTONUP:
+		if (parEvent.button.button == SDL_BUTTON_LEFT)
+			mouse.leftButtonPressed = false;
+		break;
+
+	case SDL_MOUSEMOTION:
+		mouse.posX = parEvent.motion.x;
+		mouse.posY = parEvent.motion.y;
+		break;
+	}
+}
+
+// ============================================================================
+// ----------------------------------------------------------------------------
+// ============================================================================
