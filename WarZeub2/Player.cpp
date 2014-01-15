8c7c12861002262db86b41424421f698fa667c60
Bart <gfoekfi@yahoo.com>
2014-01-15 19:30:10 +0100

    World : stub + unit declaration

--- WarZeub2/Player.cpp
+++ WarZeub2/Player.cpp
@@ -1,35 +1,13 @@
 #include "Player.h"
 #include "UserInput.h"
 #include "Util.h"
-#include "Renderer.h" //only for SCREEN_WIDTH & SCREEN_HEIGHT
+#include "World.h"
 
 // ============================================================================
 // ----------------------------------------------------------------------------
 // ============================================================================
 
 Player player;
-
-Unit grunt =
-{
-	Vec2(SCREEN_WIDTH/2, SCREEN_HEIGHT/2),
-	Vec2(SCREEN_WIDTH/2, SCREEN_HEIGHT/2),
-	DIR_N,
-	EUT_GRUNT,
-	EUS_IDLE,
-	0,
-	0
-};
-
-Unit peon =
-{
-	Vec2(SCREEN_WIDTH/4, SCREEN_HEIGHT/4),
-	Vec2(SCREEN_WIDTH/4, SCREEN_HEIGHT/4),
-	DIR_N,
-	EUT_PEON,
-	EUS_IDLE,
-	0,
-	0
-};
 
 // ============================================================================
 // ----------------------------------------------------------------------------
