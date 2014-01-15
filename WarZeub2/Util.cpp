c2c85a751a0a59cca599625cf0062a3779fbad48
Bart <gfoekfi@yahoo.com>
2014-01-15 17:01:51 +0100

    Util: Buggy bounding box collision computation

--- WarZeub2/Util.cpp
+++ WarZeub2/Util.cpp
@@ -15,5 +15,15 @@ int Clamp(int parValue, int parMin, int parMax)
 }
 
 // ============================================================================
+
+bool DoesBBoxesCollide(const SDL_Rect& parBoxA, const SDL_Rect& parBoxB)
+{
+	return (((parBoxA.x >= parBoxB.x) && (parBoxA.x <= (parBoxB.x + parBoxB.w)) ||
+		((parBoxA.x + parBoxA.w) >= parBoxB.x) && ((parBoxA.x + parBoxA.w) <= (parBoxB.x + parBoxB.w))) /*&&
+		((parBoxA.y >= parBoxB.y) && (parBoxA.y <= (parBoxB.y + parBoxB.h)) ||
+		((parBoxA.y + parBoxA.h) >= parBoxB.y) && ((parBoxA.y + parBoxA.h) <= (parBoxB.y + parBoxB.h)))*/);
+}
+
+// ============================================================================
 // ----------------------------------------------------------------------------
 // ============================================================================
